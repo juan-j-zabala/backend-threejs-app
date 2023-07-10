efe40b6b3b377dc176d64bcdfd96dbfabf068adf
juan jose zabala <juanzabala317@hotmail.com>
2023-07-10 10:30:46 -0500

    vesion1 backend

---
+++ routes/dalle.routes.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const router = express.Router()
+
+const config = new Configuration({
+})
+
+router.route("/").get((req, res) => {
+    res.status(200).json({message: "Hello from DALL.E ROUTES"})
+})
+
+export default router
